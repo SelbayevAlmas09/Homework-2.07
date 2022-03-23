8f9295913436b0982b1289043ce8b491f57fdd44
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-23 23:47:29 +0600

    Initial Commit

---
+++ Homework 2.07/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 2.07
+//
+//  Created by Almas Selbayev on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
